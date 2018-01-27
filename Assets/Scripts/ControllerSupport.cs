e395565025569fe9c3b0e4a60b6e5f010af7b781
Rufino Ansara <rufinoansara@gmail.com>
2018-01-26 20:51:11 -0500

    Added controller support with the PlayerController.

--- Assets/Scripts/ControllerSupport.cs
+++ Assets/Scripts/ControllerSupport.cs
@@ -4,43 +4,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ControllerSupport : MonoBehaviour {
+public class ControllerSupport : MonoBehaviour
+{
 
+    //global vars
     protected float x;
     protected float y;
     protected float moveSpeed = 6;
 
-    protected Rigidbody rb;
+    private PlayerController currPlayerController; 
+
+    protected Rigidbody2D rb;
     int myPlayerID;
 
-    // Use this for initialization
     void Start () {
+        //get player tag
         string myTag = this.tag;
         myPlayerID = Convert.ToInt32(myTag.Substring(myTag.Length - 1, 1));
-        rb = GetComponent<Rigidbody>();
+
+        //get player rigidbody and controller
+        rb = GetComponent<Rigidbody2D>();
+        currPlayerController = GetComponent<PlayerController>();
     }
 	
-	// Update is called once per frame
 	void Update () {
-
-        Vector3 mydir = new Vector3(x, y, 0).normalized;
+        //move the player by getting the normalized vector created by the joystick
+        Vector3 mydir = new Vector2(x, y).normalized;
         movePlayer();
-
     }
 
     void movePlayer()
     {
+        //based on the player ID, access the correct controller joystick data
         string myX = string.Concat("L_XAxis_", myPlayerID);
         string myY = string.Concat("L_YAxis_", myPlayerID);
 
+        //assign it to x and y; Ignore Y for now (no up movement)
         x = Input.GetAxis(myX);
-        y = Input.GetAxis(myY);
+        y = 0;//Input.GetAxis(myY);
 
-        Vector3 dir = new Vector3(x, y, 0);
+        Vector3 dir = new Vector3(x, y);
         dir = dir.normalized;
 
-        rb.AddForce(dir * moveSpeed);
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, 30.0f);
-
+        //this moves the player directly; do not use unless player controller is not available. 
+        /*rb.AddForce(dir * moveSpeed);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, 30.0f);*/
+    
+        currPlayerController.Move(dir);
     }
 }
