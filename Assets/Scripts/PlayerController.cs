9eeecced48d92595df892acb40395a14fe5575f2
Paden Shorey <paden.shorey@gmail.com>
2018-01-28 16:28:47 -0500

    - final changes before build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -82,6 +82,11 @@ public class PlayerController : EntityController
     {
         base.Update();
 
+        if(transform.position.y < -10f)
+        {
+            transform.position = GameManager.Instance.spawnPoints[Random.Range(0, GameManager.Instance.spawnPoints.Count)].transform.position;
+        }
+
         if (isRegenerating)
         {
             // The 0.01 is just to make it feel smoother
