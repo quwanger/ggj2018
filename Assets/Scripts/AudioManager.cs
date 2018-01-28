ad0746a305e699d0f2d6b9089d84149df49b28b7
Raiquon Welcome <raiquonwelcome@cmail.carleton.ca>
2018-01-28 10:16:51 -0500

    Basic audio manager implemented

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    static AudioManager instance = null;
+
+    public AudioClip[] coughs;
+
+    public AudioSource[] sources;
+    AudioSource soundtrack;
+
+    public GameObject audio;
+
+    void Awake ()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self-destructing");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        sources = GetComponents<AudioSource>();
+        soundtrack = sources[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
